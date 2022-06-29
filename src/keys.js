56b2ca613e01041fdb9cc8ca8f62bfea51d7d46d
neo-astro <estevanabril@hotmail.es>
2022-06-28 20:52:16 -0500

    f

---
+++ src/keys.js
@@ -0,0 +1,18 @@
+//direccion de base de datos
+module.exports = {
+    database_config: {
+        host: 'us-cdbr-east-05.cleardb.net',
+        user: 'b27639108da10b',
+        password: 'b66cfe25',
+        database: 'heroku_acde12fa964b691',
+    }
+}
+// mysql://b27639108da10b:b66cfe25@us-cdbr-east-05.cleardb.net/heroku_acde12fa964b691?reconnect=true
+// const config_db = {   
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'ADRIAN',
+//     database: 'db_links'
+// }
+
+// module.exports = config_db
